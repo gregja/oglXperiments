e5403a9cfcae3d00c168309365980d7ac13f08f6
gregja <gregory.jrrg@gmail.com>
2020-02-24 20:54:05 +0100

    upgrade on flying book (renamed flying box)

--- ogl_flying_box.js
+++ ogl_flying_box.js
@@ -1,11 +1,11 @@
-import {Renderer, Camera, Transform, Texture, Program, Box, Mesh, Vec3, Orbit} from './js/ogl/ogl.js';
+import {Renderer, Camera, Transform, Texture, Program, Box, Sphere, Mesh, Vec3, Orbit} from './js/ogl/ogl.js';
 import {vertex100, fragment100, vertex300, fragment300, render_modes, textures} from "./js/ogl_constants.js";
 {
     /*
      *  Adaptation for OGL of this sketch : http://processingjs.org/articles/RenderingModes.html
      */
     let info = document.getElementById('info');
-    info.innerHTML = "3D object : Flying book" ;
+    info.innerHTML = "3D object : Flying box" ;
 
     const {
         cos, sin, PI
@@ -17,6 +17,48 @@ import {vertex100, fragment100, vertex300, fragment300, render_modes, textures} 
     let scene, texture;
     let book, leftJoint, rightJoint;  // explicit names for parts of the shape (easier too manage)
 
+    let jointList = [
+        {
+            name: 'tinyBox',
+            type: 'box',
+            scale: [0.5, 0.5, 0.5]
+        },
+        {
+            name: 'mediumBox',
+            type: 'box',
+            scale: [1, 1, 1]
+        },
+        {
+            name: 'longBox',
+            type: 'box',
+            scale: [0.5, 5, 0.5]
+        },
+        {
+            name: 'tinySphere',
+            type: 'sphere',
+            radius: .5
+        },
+        {
+            name: 'mediumSphere',
+            type: 'sphere',
+            radius: 1
+        },
+        {
+            name: 'bigSphere',
+            type: 'sphere',
+            radius: 2
+        },
+        {
+            name: 'waowSphere',
+            type: 'sphere',
+            radius: 3
+        },
+        {
+            name: 'Not visible',
+            type: 'transform',
+        },
+    ];
+
     let settings = {
         rendering: 'TRIANGLE_STRIP',
         texture: textures[0],
@@ -25,21 +67,19 @@ import {vertex100, fragment100, vertex300, fragment300, render_modes, textures} 
         ang2:0,
         ang3:0,
         ang4:0,
-        flapSpeed:0.2
+        flapSpeed:0.2,
+        jointDefault: jointList[0].name
     };
 
     function shapeGenerator() {
-
         var skeleton = {
             position: [0, -3, 0],
             scale: [2, 5, 2],
             rotation: [0, 0, 0],
             children: [
-
-                { // left wing     rotateY(sin(radians(ang)) * -20);  rect(-75, -50, 75, 100);
+                { // left wing, in the original processing sketch => rotateY(sin(radians(ang)) * -20); rect(-75, -50, 75, 100);
                     id: "leftjoint",
                     position: [-1, 0, -0],
-                    scale: [0.5, 0.5, 0.5],
                     rotation: [0, 45, 0],
                     children: [{
                         id: "leftwing",
@@ -48,11 +88,9 @@ import {vertex100, fragment100, vertex300, fragment300, render_modes, textures} 
                         rotation: [0, 90, 0],
                     }]
                 },
-
-                { // right wing  rotateY(sin(radians(ang)) * 20);  rect(0, -50, 75, 100);
+                { // right wing, in the original processing sketch => rotateY(sin(radians(ang)) * 20); rect(0, -50, 75, 100);
                     id: "righjoint",
                     position: [1, 0, .1],
-                    scale: [0.5, 0.5, 0.5],
                     rotation: [0, -180, 0],
                     children: [{
                         id: "rightwing",
@@ -64,29 +102,52 @@ import {vertex100, fragment100, vertex300, fragment300, render_modes, textures} 
             ]
         };
 
-        const geometry = new Box(gl, {width: skeleton.scale[0], height: skeleton.scale[1], depth: skeleton.scale[2]});
+        let geometry = new Box(gl, {width: skeleton.scale[0], height: skeleton.scale[1], depth: skeleton.scale[2]});
         book =  new Mesh(gl, {mode: gl[settings.rendering], geometry, program});
         book.position.set(skeleton.position[0], skeleton.position[1], skeleton.position[2]);
         book.setParent(scene);
 
+        let jointParams = {};
+        jointList.forEach(joint => {
+            if (joint.name == settings.jointDefault) {
+                jointParams = {
+                    name: joint.name,
+                    type: joint.type,
+                    scale: joint.scale || [],
+                    radius: joint.radius || [],
+                }
+            }
+        })
+
         skeleton.children.forEach((level1, idx) => {
-            const geometry = new Box(gl, {width: level1.scale[0], height: level1.scale[1], depth: level1.scale[2]});
-            const childLvl1 =  new Mesh(gl, {mode: gl[settings.rendering], geometry, program});
+            let geometry;
+            if (jointParams.type == 'box') {
+                geometry = new Box(gl, {width: jointParams.scale[0], height: jointParams.scale[1], depth: jointParams.scale[2]});
+            } else {
+                if (jointParams.type == 'sphere') {
+                    geometry = new Sphere(gl, {radius: jointParams.radius});
+                } else {
+                    geometry = new Transform(gl);
+                }
+            }
+            let childLvl1 =  new Mesh(gl, {mode: gl[settings.rendering], geometry, program});
             childLvl1.position.set(level1.position[0], level1.position[1], level1.position[2]);
             childLvl1.rotation.set(level1.rotation[0], level1.rotation[1], level1.rotation[2]);
-            childLvl1.setParent(book);
+            book.addChild(childLvl1);
+            //childLvl1.setParent(book);
             if (idx == 0) {
                 leftJoint = childLvl1;
             } else {
                 rightJoint = childLvl1;
             }
             level1.children.forEach(level2 => {
-                console.log(level2)
-                const geometry = new Box(gl, {width: level2.scale[0], height: level2.scale[1], depth: level2.scale[2]});
-                const childLvl2 =  new Mesh(gl, {mode: gl[settings.rendering], geometry, program});
+                //console.log(level2)
+                let geometry = new Box(gl, {width: level2.scale[0], height: level2.scale[1], depth: level2.scale[2]});
+                let childLvl2 =  new Mesh(gl, {mode: gl[settings.rendering], geometry, program});
                 childLvl2.position.set(level2.position[0], level2.position[1], level2.position[2]);
                 childLvl2.rotation.set(level2.rotation[0], level2.rotation[1], level2.rotation[2]);
-                childLvl2.setParent(childLvl1);
+                //childLvl2.setParent(childLvl1);
+                childLvl1.addChild(childLvl2);
             });
         });
     }
@@ -224,6 +285,15 @@ import {vertex100, fragment100, vertex300, fragment300, render_modes, textures} 
                     texture = new Texture(gl);
                     program.uniforms.tMap = {value: texture};
                 }
+            }
+        });
+
+        let guiJoint = gui.add(obj, 'jointDefault', jointList.map(item=>item.name), obj.jointDefault).listen();  // none by default
+        guiJoint.onChange(function(value){
+            if (obj.jointDefault != null) {
+                obj.jointDefault = value;
+                scene = new Transform();
+                shapeGenerator();
             }
         });
 
